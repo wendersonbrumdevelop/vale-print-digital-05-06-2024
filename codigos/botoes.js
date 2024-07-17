4c9f788a2606c0a238d11b082a2e3d70f06c4941
JulianaElen <julianaesoliveira@gmail.com>
2024-07-16 22:38:39 -0300

    Finalização da pagina principal

---
+++ codigos/botoes.js
@@ -0,0 +1,4 @@
+// Adiciona um evento de clique ao botão de contato
+document.getElementById('botaoContato').addEventListener('click', function() {
+    window.location.href = 'contato.html'; // Redireciona para a página de contato
+});
